c9765b30c94dd3d037056d6dd625056629c888bd
Roman <Nuadire@gmail.com>
2020-07-14 17:27:08 +0500

    Updates

---
+++ precache-manifest.958167f3eb9a44d3dddf28da202a9cfa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14ba658d25ece9526ed59c2c12e4a77",
+    "url": "/Registration/index.html"
+  },
+  {
+    "revision": "2873d4d0923136c855b0",
+    "url": "/Registration/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "524fc9c688ed212d9c02",
+    "url": "/Registration/static/css/main.a41ea0d5.chunk.css"
+  },
+  {
+    "revision": "2873d4d0923136c855b0",
+    "url": "/Registration/static/js/2.52af7513.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Registration/static/js/2.52af7513.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "524fc9c688ed212d9c02",
+    "url": "/Registration/static/js/main.16dec94b.chunk.js"
+  },
+  {
+    "revision": "a5ec90830405b396337a",
+    "url": "/Registration/static/js/runtime-main.cb2a724d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Registration/precache-manifest.eed7478961f4a5d6f42a7200aad0cc05.js"
+  "/Registration/precache-manifest.958167f3eb9a44d3dddf28da202a9cfa.js"
 );
 
 self.addEventListener('message', (event) => {
