7a72aff8fa150d07f7f73b2af484ad2c8e84f4ba
Roman <Nuadire@gmail.com>
2020-07-14 11:58:57 +0500

    Updates

---
+++ precache-manifest.c02958cd9accdab91a86a532b49cb616.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5121296ee90393f4921a983dd673351",
+    "url": "/registration/index.html"
+  },
+  {
+    "revision": "b1f39c30f2f2128b28b9",
+    "url": "/registration/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "1774affb2480a9c8ee7a",
+    "url": "/registration/static/css/main.8cc61f0a.chunk.css"
+  },
+  {
+    "revision": "b1f39c30f2f2128b28b9",
+    "url": "/registration/static/js/2.e5503cae.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/registration/static/js/2.e5503cae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1774affb2480a9c8ee7a",
+    "url": "/registration/static/js/main.980bc8e9.chunk.js"
+  },
+  {
+    "revision": "d4c71797a714caf6fbbb",
+    "url": "/registration/static/js/runtime-main.41f4b2c5.js"
+  }
+]);
